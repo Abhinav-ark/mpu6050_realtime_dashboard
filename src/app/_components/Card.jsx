15b80aafb6b6e757023805a38f3c1068cadbc6ca
Abhinav R <76396917+Abhinav-ark@users.noreply.github.com>
2024-04-18 07:45:16 +0530

    Added multiline graph

--- src/app/_components/Card.jsx
+++ src/app/_components/Card.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Card = ({val}) => {
+const Card = ({val,type,bg,text}) => {
   return (
-    <div className='bg-blue-100 m-10 w-32 h-32 rounded-3xl flex flex-col items-center justify-center drop-shadow-lg'>
-        <h1 className='font-semibold text-[#1F51FF]'>Current Value</h1>
-        <h1 className="text-6xl font-semibold text-[#1F51FF]">{val}</h1>
+    <div className={`${bg} m-10 w-32 h-32 rounded-3xl flex flex-col items-center justify-center drop-shadow-lg`}>
+        <h1 className={`font-semibold ${text}`}>Current {type}</h1>
+        <h1 className={`text-5xl font-semibold ${text}`}>{val}</h1>
     </div>
   );
 }
